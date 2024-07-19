54e9afc5f0af90b07f49c22036b12cc475b37923
Keegan Leary <35752455+learyjk@users.noreply.github.com>
2024-07-18 18:04:39 -0700

    displays patterns

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,99 @@
 <script lang="ts">
-  let count = 0;
+  let selectedPatternKey: string;
+
+  type Pattern = {
+    name: string;
+    backgroundImage: string;
+  };
+
+  const patterns: { [key: string]: Pattern } = {
+    "pattern-1": {
+      name: "Horizontal",
+      backgroundImage:
+        "linear-gradient(0deg, rgb(0, 0, 0) 50%, rgb(255, 255, 255) 50%)",
+    },
+    "pattern-2": {
+      name: "Vertical",
+      backgroundImage:
+        "linear-gradient(to right, rgb(0,0,0), rgb(0,0,0) 5px, rgb(255,255,255) 5px, rgb(255,255,255))",
+    },
+    "pattern-3": {
+      name: "Diagonal",
+      backgroundImage:
+        "linear-gradient(45deg, rgb(0, 0, 0) 25%, rgb(255, 255, 255) 25%, rgb(255, 255, 255) 50%, rgb(0, 0, 0) 50%, rgb(0, 0, 0) 75%, rgb(255, 255, 255) 75%, rgb(255, 255, 255))",
+    },
+  };
+
+  function handlePatternButtonClick(event: Event, patternKey: string) {
+    const pattern = patterns[patternKey];
+
+    selectedPatternKey = patternKey;
+
+    const patternSection =
+      document.querySelector<HTMLDivElement>(".section-pattern");
+    if (!patternSection) return;
+    patternSection.style.backgroundImage = pattern.backgroundImage;
+    patternSection.style.backgroundSize = "10px 10px";
+  }
 </script>
 
 <main>
-  <button on:click={() => (count += 1)}>
-    Clicked {count}
-    {count === 1 ? "time" : "times"}
-  </button>
+  <section class="section-pattern">
+    <div class="container">
+      <div class="pattern-list">
+        {#each Object.keys(patterns) as patternKey}
+          <button
+            class="pattern-button"
+            on:click={(event) => handlePatternButtonClick(event, patternKey)}
+          >
+            <div
+              class="pattern-box"
+              style="background-image: {patterns[patternKey].backgroundImage}"
+            >
+              <h2 class="pattern-name">{patterns[patternKey].name}</h2>
+            </div>
+          </button>
+        {/each}
+      </div>
+    </div>
+  </section>
 </main>
 
 <style>
+  .container {
+    padding: 1rem;
+  }
+  .pattern-list {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    gap: 1rem;
+  }
+  .pattern-button {
+    width: 100%;
+    display: flex;
+    padding: 0px;
+    background: none;
+    border: none;
+    cursor: pointer;
+    transition: transform 0.2s ease-in-out;
+    border-radius: 4px;
+    overflow: hidden;
+  }
+  .pattern-box {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: end;
+    align-items: center;
+    aspect-ratio: 1 / 1;
+    background-size: 10px 10px;
+    padding-bottom: 0.5rem;
+  }
+  .pattern-name {
+    padding: 0.25rem;
+    font-size: small;
+    background-color: white;
+    color: black;
+    border-radius: 4px;
+  }
 </style>
