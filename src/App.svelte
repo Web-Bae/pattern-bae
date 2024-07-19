bce11a6928a8c4d8c13af7a95bb16c71fdfc1ce1
Keegan Leary <35752455+learyjk@users.noreply.github.com>
2024-07-18 17:47:52 -0700

    initial setup complete

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  let count = 0;
+</script>
+
+<main>
+  <button on:click={() => (count += 1)}>
+    Clicked {count}
+    {count === 1 ? "time" : "times"}
+  </button>
+</main>
+
+<style>
+</style>
---
+++ types.d.ts
@@ -0,0 +1,1 @@
+declare module "@xatom/wf-app-hot-reload";
